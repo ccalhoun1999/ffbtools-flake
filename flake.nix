3b55d3571e3a4d9dc305cdf65fc6ba5153ff88e8
Charles Calhoun <ccalhoun1999@gmail.com>
2024-10-24 15:02:37 +0900

    ffbtools flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "ffbtools";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages = rec {
+          default = ffbtools;
+          ffbtools = pkgs.stdenv.mkDerivation {
+            pname = "ffbtools";
+            version = "master";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "berarma";
+              repo = "ffbtools";
+              rev = "master";
+              sha256 = "K9RdZ211nhzQhNaw+vPx6XZ047ERjU++eUyaMyyEl70=";
+            };
+
+            installPhase = ''
+              # cd bin
+              # ls
+              runHook preInstall
+              mkdir -p $out/bin
+              cp bin/* $out/bin
+              runHook postInstall
+            '';
+          };
+        };
+      }
+    );
+}
